3e756c4953225dd32d03b917323c75b452c2975f
Luis Arenas <luis.arenas911@gmail.com>
2021-10-13 10:40:11 +0200

    "add arrayPractice and fullName exercises"

---
+++ arrayPractice.js
@@ -0,0 +1,26 @@
+// arrayPractice.js
+
+// importing our array of patients using `require`
+const patients = require("./patients");
+
+//console.log(patients);
+
+// accesing elements in an array
+const firstPatient = patients[0];
+
+console.log(firstPatient);
+
+// which element to access can also be dependent on a variable
+const whichElementOfArray = 9;
+const tenthPatient = patients[whichElementOfArray];
+// const tenthPatient = patients[9];
+
+console.log(tenthPatient);
+
+// DIY
+
+// - console.log the second patient from the array
+
+console.log(patients[1])
+// - console.log the last patient from the array
+console.log(patients[patients.length-1])
