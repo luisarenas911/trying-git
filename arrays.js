28ca108fcbca678b705adeba05ca25e2c70e1685
Luis Arenas <luis.arenas911@gmail.com>
2021-10-13 10:35:06 +0200

    Add files with array and emalilist exercises

---
+++ arrays.js
@@ -0,0 +1,13 @@
+
+//objjecrts
+const name "Matias";
+const age = 30
+const gender = "M"
+const nationality = "URU"
+
+const student = {
+    name: "Matias",
+    age: 30,
+    gender: "M",
+    nationality: "URU"
+}
